beaf9a36ed79ba35241edba34e70fda91726ded0
sagarpawar01 <74352362+sagarpawar01@users.noreply.github.com>
2024-05-16 16:51:09 +0530

    Update vite.config.js

--- frontends/vite.config.js
+++ frontends/vite.config.js
@@ -6,8 +6,8 @@ export default defineConfig({
   plugins: [react()],
   server : {
     proxy : {
-      "/api/" : "http://localhost:5000",
-      "/uploads/" : "http://localhost:5000",
+      "/api/" : "https://mern-app-1-e7mz.onrender.com",
+      "/uploads/" : "https://mern-app-1-e7mz.onrender.com",
     }
   }
 })
